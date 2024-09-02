edd72b0f5e8f1701a82dd1092240b20c5bcc280e
Davis Hammon <davis@risingtiger.com>
2024-09-02 12:55:46 -0600

    got target out of repo

--- buildit/target/debug/build/rayon-core-cba18d45bf387724/build_script_build-cba18d45bf387724.d
+++
@@ -1,5 +0,0 @@
-/Users/dave/Code/nifty/buildit/target/debug/build/rayon-core-cba18d45bf387724/build_script_build-cba18d45bf387724: /Users/dave/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rayon-core-1.12.1/build.rs
-
-/Users/dave/Code/nifty/buildit/target/debug/build/rayon-core-cba18d45bf387724/build_script_build-cba18d45bf387724.d: /Users/dave/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rayon-core-1.12.1/build.rs
-
-/Users/dave/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rayon-core-1.12.1/build.rs:
