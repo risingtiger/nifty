fa90bb9fd8f71a4a6ee834678dbef4e93ba19909
Davis Hammon <davis@risingtiger.com>
2024-09-09 22:45:14 -0600

    added xen submodule

---
+++ server/src/server_xen/CHANGELOG.md
@@ -0,0 +1,13 @@
+
+
+## 1.0.0 -- UNRELEASED
+
+### FEATURES
+- first version of Finance complete
+
+### ENHANCEMENTS
+- ...
+
+### BUGFIXES
+- ...
+
---
+++ server/src/server_xen/admin/admin_firestore.ts
@@ -0,0 +1,72 @@
+
+
+//type str = string; //type int = number; type bool = boolean;
+
+import {FieldValue} from "@google-cloud/firestore"
+//import fs from "fs";
+
+
+
+
+const Misc_Update = async (db: any) => {
+
+    return new Promise<any>(async (res, _rej)=> {
+
+        const collection = db.collection("transactions")
+        const snapshot = await collection.get()
+        const items = snapshot.docs.map((m: any) => ({ id: m.id, ...m.data() }));
+
+        let batch        = db.batch()
+
+        for (const t of items) {
+
+            const updateobj = {
+                ynab_id: ""
+            }
+
+            batch.update(collection.doc(t.id), updateobj);
+        }
+
+        await batch.commit().catch((er:any)=> console.error(er))
+
+        res({return_str:"Done Misc Update"})
+  })
+
+}
+
+
+
+
+const Misc_Get = async (db: any) => {
+
+    return new Promise<any>(async (res, _rej)=> {
+        
+        const cats_collection = db.collection("cats")
+
+        const cats_snapshot = await cats_collection.get()
+
+        const cats = cats_snapshot.docs.map((m: any) => ({ id: m.id, ...m.data() }));
+
+        let objarray:any[] = []
+
+        for(const cat of cats){
+
+            if (cat.name.includes("ya")) {
+                objarray.push({
+                    name: "chipaya",
+                })
+            }
+        }
+
+        console.log(JSON.stringify(objarray, null, 2))
+
+        res(objarray)
+    })
+}
+
+
+
+
+const Admin_Firestore = { Misc_Update, Misc_Get };
+
+export default Admin_Firestore;
