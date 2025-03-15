bd020cd9ad5aa5a74a9d89a6d93bab5972086fe2
Davis Hammon <accounts@risingtiger.com>
2025-03-15 17:45:16 -0600

    feat: Add switchstation module to alwaysload client directory

---
+++ client/alwaysload/switchstation.ts
@@ -0,0 +1,272 @@
+
+
+import { $NT, LazyLoadT, FirestoreLoadSpecT, URIDetailT, LoggerSubjectE } from  "./../defs.js" 
+import { str, num } from  "../../defs_server_symlink.js" 
+import { Run as LazyLoadRun } from './lazyload.js'
+import { AddView as CMechAddView } from "./cmech.js"
+import { RegExParams, GetLoadSpec } from "./switchstation_uri.js"
+
+declare var $N: $NT;
+
+let isSwipeBackGesture   = false;
+let _routes:Array<Route> = [];
+
+
+
+
+
+
+class Route {
+
+    lazyload_view: LazyLoadT
+    path_regex: RegExp
+    path_paramnames: Array<str>
+
+
+
+
+    constructor(lazyload_view_: LazyLoadT) {
+        this.lazyload_view = lazyload_view_
+
+		const {regex, paramnames} = RegExParams(this.lazyload_view.urlmatch!)
+
+        this.path_regex      = regex
+        this.path_paramnames = paramnames
+    }
+
+
+
+
+    load(uri:str, urlmatches:Array<str>, views_attach_point:"beforeend"|"afterbegin") { 
+
+        return new Promise<string>( async (res, _rej) => {
+
+			const loadspecs = GetLoadSpec(uri, urlmatches, this.path_paramnames, this.lazyload_view.loadspecs!)
+
+			const promises:Promise<any>[] = []
+
+            promises.push( LazyLoadRun([this.lazyload_view]) )
+			promises.push( CMechAddView(this.lazyload_view.name, loadspecs, views_attach_point) )
+
+			const r = await Promise.all(promises)
+
+			if (r[0] === null || r[1] === null) { res('failed'); return; }
+
+			res('success')
+        })
+    }
+}
+
+
+
+
+
+
+
+
+const Init = async ()=> {
+
+    const initialPath = window.location.pathname.slice(3)
+
+    if (!history.state || history.state.index === undefined) {
+		await routeChanged(initialPath, 'firstload');
+        history.replaceState({ index: 0, path: initialPath }, '', initialPath);
+    } else {
+		await routeChanged(history.state.path, 'firstload');
+    }
+
+
+	window.addEventListener("touchstart", (e: TouchEvent) => {
+		const touch = e.touches[0];
+		if (touch.clientX < 50 && touch.clientY > 60) {
+			isSwipeBackGesture = true;
+		}
+	})
+
+
+	window.addEventListener("touchmove", (_e: TouchEvent) => {
+	})
+
+
+	window.addEventListener("touchend", () => {
+		setTimeout(() => isSwipeBackGesture = false, 600)
+	})
+
+
+	window.addEventListener("popstate", async (e) => {
+		if (e.state) routeChanged(e.state.path, 'back')
+	})
+}
+
+
+
+
+const AddRoute = (lazyload_view:LazyLoadT)=> {
+    _routes.push(new Route(lazyload_view))
+}
+
+
+
+
+async function NavigateTo(newPath: string) {
+    const r = await routeChanged(newPath, 'forward');
+	if (r === null) { 
+		$N.ToastShow("Couldnt navigate to page", 4, 5000000)
+		return; 
+	}
+
+    history.pushState({ index: history.state.index+1, path: newPath }, '', newPath);
+}
+
+
+
+
+async function NavigateBack(opts:{ default:str}) {
+
+	if (history.state && history.state.index > 0) {
+		await routeChanged(opts.default, 'back');
+		history.back();
+	}
+	else {
+		await routeChanged(opts.default, 'back');
+		history.replaceState({ index: 0, path: opts.default }, '', opts.default);
+	}
+}
+
+
+
+
+const routeChanged = (path: string, direction:'firstload'|'back'|'forward' = 'firstload') => new Promise<num|null>(async (res, _rej) => {
+
+	const viewsel            = document.getElementById("views") as HTMLElement;
+
+    const [urlMatches, routeObj] = get_route_uri(path);
+
+    if (direction === "firstload") {
+
+		const loadresult = await routeObj.load(path, urlMatches, "beforeend");
+
+		if (loadresult === 'failed') {
+			$N.Unrecoverable("Error", "Could Not Load Page", "Reset App", `/index.html?error_subject=${LoggerSubjectE.switch_station_route_load_fail}`)
+			res(null);
+			return;
+		}
+
+		( viewsel.children[0] as HTMLElement ).style.display = "block";
+
+		document.querySelector("#views")!.dispatchEvent(new Event("visibled"));
+    }
+
+    else if (direction === "forward") {
+
+		const loadresult = await routeObj.load(path, urlMatches, "beforeend");
+
+		if (loadresult === 'failed') {
+			res(null);
+			return;
+		}
+
+        const activeview = viewsel.children[viewsel.children.length - 1] as HTMLElement;
+        activeview.classList.add("next_startstate");
+        activeview.style.display = "block";
+        activeview.offsetHeight; // force reflow
+        activeview.classList.remove("next_startstate");
+
+        const previousview = activeview.previousElementSibling as HTMLElement;
+        if (previousview) {
+            previousview.classList.add("previous_endstate");
+        }
+
+        activeview.addEventListener("transitionend", function activeTransitionEnd() {
+            if (previousview) {
+                previousview.style.display = "none";
+            }
+            activeview.removeEventListener("transitionend", activeTransitionEnd);
+
+			document.querySelector("#views")!.dispatchEvent(new Event("visibled"));
+        });
+    }
+
+    else if (direction === "back") {
+
+        const activeview = viewsel.children[viewsel.children.length - 1] as HTMLElement;
+        let previousview = activeview?.previousElementSibling as HTMLElement;
+
+
+        if (isSwipeBackGesture) {
+            activeview.remove();
+			previousview.classList.remove("previous_endstate");
+			await new Promise((res, _rej) => setTimeout(res, 100));
+			previousview.style.display = "block";
+            document.querySelector("#views")!.dispatchEvent(new Event("view_load_done"));
+            res(1);
+            return;
+        }
+
+        if (!previousview) {
+			const loadresult = await routeObj.load(path, urlMatches, "afterbegin");
+            if (loadresult === "failed") {
+				$N.Unrecoverable("Error", "Could Not Load Page", "Reset App", `/index.html?error_subject=${LoggerSubjectE.switch_station_route_load_fail}`)
+				res(null);
+				return;
+            }
+            previousview = activeview?.previousElementSibling as HTMLElement;
+        }
+
+		previousview.style.display = "block";
+        activeview.offsetHeight; // force reflow
+        activeview.classList.add("active_endstate");
+        previousview.classList.remove("previous_endstate");
+
+        activeview.addEventListener("transitionend", function activeTransitionEnd() {
+            activeview.remove();
+            const previous_previousview = previousview?.previousElementSibling as HTMLElement;
+            if (previous_previousview) {
+            }
+            activeview.removeEventListener("transitionend", activeTransitionEnd);
+
+			document.querySelector("#views")!.dispatchEvent(new Event("visibled"));
+        });
+    }
+
+	res(1);
+})
+
+
+
+
+function get_route_uri(url: str) : [Array<str>, Route] {
+
+    for (let i = 0; i < _routes.length; i++) {
+
+		let urlmatchstr = url.match(_routes[i].path_regex)
+
+		if (urlmatchstr) { 
+			return [ urlmatchstr.slice(1), _routes[i] ]
+		}
+    }
+
+    // catch all -- just route to home
+    return [ [], _routes.find(r=> r.lazyload_view.name==="home")! ]
+}
+
+
+
+
+export { Init, AddRoute }
+
+if (!(window as any).$N) {   (window as any).$N = {};   }
+((window as any).$N as any).SwitchStation = { NavigateTo, NavigateBack };
+
+
+
+
+
+
+
+
+
+
+
+
+
