fcd36110dccc1889c764b15d96b21b4bf8439de3
Davis Hammon <accounts@risingtiger.com>
2025-03-19 09:29:12 -0600

    refactor: Update route handling and add new IndexedDB utility functions

--- client/alwaysload/switchstation.ts
+++ client/alwaysload/switchstation.ts
@@ -22,14 +22,15 @@ let _routes:Array<Route> = [];
 
 const Init = async ()=> {
 
+	const pathname     = window.location.pathname.slice(3)
     const searchParams = window.location.search ? window.location.search : '';
-    const initialPath = window.location.pathname.slice(3) + searchParams;
+    const initialPath  = window.location.pathname + searchParams;
 
     if (!history.state || history.state.index === undefined) {
-		await routeChanged(initialPath, 'firstload');
-        history.replaceState({ index: 0, path: initialPath }, '', initialPath);
+		await routeChanged(pathname, 'firstload');
+        history.replaceState({ index: 0, path: initialPath  }, '', initialPath);
     } else {
-		await routeChanged(history.state.path, 'firstload');
+		await routeChanged(pathname, 'firstload');
     }
 
 
@@ -109,7 +110,7 @@ async function NavigateToSearchParams(newsearchparams:GenericRowT) {
 
 	const searchparams_str = searchparams.toString();
 
-	const newhistoryurl = window.location.pathname.slice(3) + '?' + searchparams_str;
+	const newhistoryurl = window.location.pathname + '?' + searchparams_str;
 
     history.pushState({ index: history.state.index+1, path: newhistoryurl }, '', newhistoryurl);
     
@@ -282,23 +283,206 @@ if (!(window as any).$N) {   (window as any).$N = {};   }
 
 
 
-const loady_a = (_pathparams:GenericRowT, _searchparams: URLSearchParams) => new Promise<Map<str,GenericRowT[]>>(async (res, _rej) => {
-
+const finance_loady_a = (_pathparams:GenericRowT, _searchparams: URLSearchParams) => new Promise<null|Map<str,GenericRowT[]>>(async (res, _rej) => {
 	const a = new Map<str,GenericRowT[]>()
-
 	a.set("testy1", [{testy1:1}, {testy1:2}])
 	a.set("testy2", [{testy2:10}, {testy2:20}])
+	res(a)	
+})
+
+const finance_loady_b = (_pathparams:GenericRowT, _old_searchparams: URLSearchParams, _new_searchparams: URLSearchParams) => new Promise<Map<str,GenericRowT[]>>(async (res, _rej) => {
+	const a = new Map<str,GenericRowT[]>()
+	a.set("b_testy1", [{testy1:1}, {testy1:2}])
+	a.set("b_testy2", [{testy2:10}, {testy2:20}])
+	res(a)
+})
+
+
+
+
+const addtr_loady_a = (_pathparams:GenericRowT, _searchparams: URLSearchParams) => new Promise<null|Map<str,GenericRowT[]>>(async (res, _rej) => {
+	const a = new Map<str,GenericRowT[]>()
+
+	const ynabraw = await $N.FetchLassie('/api/xen/finance/get_ynab_raw_transactions', {})
+	if (ynabraw === null) { res(null); return; }
+
+	a.set("ynabraw", ynabraw as GenericRowT[])
 
 	res(a)	
 })
 
-const loady_b = (_pathparams:GenericRowT, _old_searchparams: URLSearchParams, _new_searchparams: URLSearchParams) => new Promise<Map<str,GenericRowT[]>>(async (res, _rej) => {
-
+const addtr_loady_b = (_pathparams:GenericRowT, _old_searchparams: URLSearchParams, _new_searchparams: URLSearchParams) => new Promise<Map<str,GenericRowT[]>>(async (res, _rej) => {
 	const a = new Map<str,GenericRowT[]>()
-
 	a.set("b_testy1", [{testy1:1}, {testy1:2}])
 	a.set("b_testy2", [{testy2:10}, {testy2:20}])
-
 	res(a)
 })
+
+
+
+
+
+
+const getAllPromise = (objectStore:IDBObjectStore) => new Promise((res, rej) => {
+	const request = objectStore.getAll();
+	request.onsuccess = (ev:any) => res(ev.target.result);
+	request.onerror   = (ev:any) => rej(ev.target.error);
+})
+
+
+const indexeddb_graball = (objectstore_names:str[]) => new Promise<Map<str,GenericRowT[]>|null>(async (res, _rej) => {
+
+	const db = await openindexeddb()
+
+	const returns:Map<str,GenericRowT[]> = new Map<str,GenericRowT[]>() // key being the objectstore name
+
+	const transaction             = db.transaction(objectstore_names, 'readonly');
+
+	const promises:Promise<any>[] = []
+
+	for (const objectstore_name of objectstore_names) {
+		const objectstore = transaction.objectStore(objectstore_name)
+		promises.push(getAllPromise(objectstore))
+	}
+
+	const r = await Promise.all(promises)
+		.catch((_e) => { res(null); return null; })
+
+	const transaction_store       = transaction.objectStore('transactions');
+	const cat_store               = transaction.objectStore('cats');
+	const source_store            = transaction.objectStore('sources');
+
+	const t1 = performance.now()
+
+	const cats_request    = cat_store.getAll();
+	const sources_request = source_store.getAll();
+	
+	const catsPromise = new Promise<any[]>((resolveC, rejectC) => {
+		cats_request.onsuccess = () => {
+			resolveC(cats_request.result);
+		};
+		cats_request.onerror = () => {
+			rejectC('Error fetching cats');
+		};
+	});
+
+	const sourcesPromise = new Promise<any[]>((resolveC, rejectC) => {
+		sources_request.onsuccess = () => {
+			resolveC(sources_request.result);
+		};
+		sources_request.onerror = () => {
+			rejectC('Error fetching sources');
+		};
+	});
+
+	let count = 0;
+	transaction_store.openCursor().onsuccess = (event) => {
+		const cursor = (event.target as IDBRequest<IDBCursorWithValue>).result;
+		
+		if (cursor && count < 100) {
+			const transaction = cursor.value;
+			transactions.push(transaction);
+			count++;
+			cursor.continue();
+		} else {
+
+			Promise.all([catsPromise, sourcesPromise]).then((results) => {
+				const t2 = performance.now()
+				console.log("100testdb_a time: " + (t2 - t1));
+				cats = results[0];
+				sources = results[1];
+				resolve({transactions, cats, sources});
+			}).catch(error => {
+				console.error(error);
+				reject(error);
+			});
+		}
+	}
+})
+
+
+
+
+const testdb_b = () => new Promise(async (resolve, reject) => {
+
+	const db = await openindexeddb()
+
+	let  transactions: any[]      = [];
+	let  cats:any[]               = [];
+	let  sources:any[]            = [];
+	let  cat_ids:Set<string>      = new Set<string>();
+	let  source_ids:Set<string>   = new Set<string>();
+        
+	const transaction             = db.transaction(['transactions', 'cats','sources'], 'readonly');
+	const transaction_store       = transaction.objectStore('transactions');
+	const cat_store               = transaction.objectStore('cats');
+	const source_store            = transaction.objectStore('sources');
+
+	const t1 = performance.now()
+
+	transaction_store.openCursor().onsuccess = (event) => {
+		const cursor = (event.target as IDBRequest<IDBCursorWithValue>).result;
+		
+		if (cursor) {
+			const transaction = cursor.value;
+			transactions.push(transaction);
+			
+			// Process cat if not already processed
+			if (!cat_ids.has(transaction.cat)) {
+				const cat_request = cat_store.get(transaction.cat);
+				
+				cat_request.onsuccess = () => {
+					cats.push(cat_request.result)	
+					cat_ids.add(transaction.cat)
+				};
+				
+				cat_request.onerror = () => {
+					console.error('Error retrieving cat:', cat_request.error);
+				};
+			}
+			
+			// Process source if not already processed
+			if (!source_ids.has(transaction.source)) {
+				const source_request = source_store.get(transaction.source);
+				
+				source_request.onsuccess = () => {
+					sources.push(source_request.result)	
+					source_ids.add(transaction.source)
+				};
+				
+				source_request.onerror = () => {
+					console.error('Error retrieving source:', source_request.error);
+				};
+			}
+
+			cursor.continue();
+		} else {
+			const t2 = performance.now()
+			console.log("-testdb_b time: " + (t2 - t1));
+
+			resolve({transactions, cats, sources});
+		}
+	}
+})
+
+
+
+
+const openindexeddb = () => new Promise<IDBDatabase>(async (res,_rej)=> {
+
+	let dbconnect = indexedDB.open('xenition', 4)
+
+	dbconnect.onerror = (event:any) => { 
+		console.log("IndexedDB Error - " + event.target.errorCode)
+	}
+
+	dbconnect.onsuccess = async (event: any) => {
+		event.target.result.onerror = (event:any) => {
+			console.log("IndexedDB Error - " + event.target.errorCode)
+		}
+		const db = event.target.result
+		res(db)
+	}
+})
+
 
